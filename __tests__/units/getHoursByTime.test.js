7568b36471a892bc3a71b56ec9c285549837e07d
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2020-01-24 11:52:15 -0300

    feat: Add tests structure
    * Add and set jest library to make tests in the application

---
+++ __tests__/units/getHoursByTime.test.js
@@ -0,0 +1,3 @@
+it('should be sum two numbers', () => {
+  expect(1 + 1).toBe(2);
+});
--- src/server.js
+++ src/server.js
@@ -1,1 +1,3 @@
-console.log('Hello nata.house !');
+import app from '~/app';
+
+app.listen(3333, () => console.log('Server running...'));
